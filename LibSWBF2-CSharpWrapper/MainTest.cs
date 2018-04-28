a80afaceed2b250721d3512ce139741eaa13f3b2
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2018-04-28 16:44:38 +0200

    Console Test Program

---
+++ LibSWBF2-CSharpWrapper/MainTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibSWBF2.Types;
+
+namespace LibSWBF2
+{
+    static class MainTest
+    {
+        static void PrintLastLines()
+        {
+            string[] lines = Log.GetLatest(ELogType.Info);
+
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+
+        static void Main(string[] args)
+        {
+            Log.Add("Test1", ELogType.Info);
+            Log.Add("Test2fdgdfg", ELogType.Info);
+            Log.Add("Testdfgd3", ELogType.Info);
+            Log.Add("Test4", ELogType.Info);
+            Log.Add("Test55555555", ELogType.Info);
+
+            PrintLastLines();
+
+            Color color = new Color(0.5f, 0.5f, 0.5f);
+
+            PrintLastLines();
+
+            Console.WriteLine(color.R);
+
+            Console.ReadKey();
+        }
+    }
+}
