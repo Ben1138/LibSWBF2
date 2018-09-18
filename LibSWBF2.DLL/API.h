887088455d8ef93a67c064eedaf8953d4e14a047
Ben1138 <marten.mildt@web.de>
2018-09-18 22:41:19 +0200

    FIX: Added missing gitignore exception for LibSWBF2.DLL folder, which prevented the whole Project from being pushed... duh! (because of global *.dll ignore rule). Pushed the LibSWBF2.DLL project.

---
+++ LibSWBF2.DLL/API.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "../LibSWBF2/Logger.h"
+#include "../LibSWBF2/LoggerEntry.h"
+#include "../LibSWBF2/Color.h"
+#include "../LibSWBF2/Vector2.h"
+#include "../LibSWBF2/Vector3.h"
+#include "../LibSWBF2/Vector4.h"
+#include "../LibSWBF2/MSH.h"
+
+#ifdef LIBSWBF2DLL_EXPORTS
+#define LIBSWBF2_API __declspec(dllexport)
+#else
+#define LIBSWBF2_API __declspec(dllimport)
+#endif
+
+namespace LibSWBF2::API
+{
+	// be carefull in the futute!
+	// there might be multiple chunks with the same m_NAME
+	using namespace Chunks::Mesh;
+	using namespace Types;
+	using namespace Logging;
+
+	extern "C"
+	{
+		LIBSWBF2_API MSH* MSH_LoadFromFile(const char* path);
+		LIBSWBF2_API void MSH_Delete(MSH* ptr);
+
+		LIBSWBF2_API void AddLogMessage(const char* message, const ELogType logType);
+	}
+}
