d5ba2e0031a8696c486cfd0e18a080776de03081
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2018-10-06 14:08:16 +0200

    Introducing requirement and library header files

---
+++ LibSWBF2.DLL/LibSWBF2API.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "req.h"
+#include "API.h"
+
+// Include this file to dynamically link against LibSWBF2
+// This header should not be included in its own project
---
+++ LibSWBF2/LibSWBF2.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "req.h"
+#include "Logger.h"
+#include "LoggerEntry.h"
+#include "Color.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+#include "BoneWeight.h"
+#include "MSH.h"
+
+// Include this file to statically link against LibSWBF2
+// This header should not be included in its own project
