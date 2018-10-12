7c4fad7a4fba5316f58bcd0b0cd9c5ad2dfe5458
Ben1138 <marten.mildt@web.de>
2018-10-12 16:22:41 +0200

    Adapted NET wrapper project

--- LibSWBF2.NET/MSH.cpp
+++ LibSWBF2.NET/MSH.cpp
@@ -3,8 +3,35 @@
 
 namespace LibSWBF2NET::Mesh
 {
-	MSH::MSH() : ManagedObject(new NativeMSH())
+	MSH::MSH()
 	{
-		
+		msh = NativeMSH::Create();
+	}
+
+	MSH::~MSH()
+	{
+		this->!MSH();
+	}
+
+	MSH::!MSH()
+	{
+		if (msh != nullptr)
+		{
+			NativeMSH::Destroy(msh);
+		}
+	}
+	bool MSH::WriteToFile(String^ Path)
+	{
+		return msh->WriteToFile(MarshalString(Path));
+	}
+
+	bool MSH::ReadFromFile(String^ Path)
+	{
+		return msh->ReadFromFile(MarshalString(Path));
+	}
+
+	int MSH::ShadowVolume::get()
+	{
+		return msh->m_SHVO.m_ShadowVolume;
 	}
 }
--- LibSWBF2.NET/MSH.h
+++ LibSWBF2.NET/MSH.h
@@ -5,27 +5,18 @@ namespace LibSWBF2NET::Mesh
 {
 	typedef LibSWBF2::Chunks::Mesh::MSH NativeMSH;
 
-	public ref class MSH : ManagedObject<NativeMSH>
+	public ref class MSH
 	{
+	private:
+		NativeMSH* msh;
+
 	public:
 		MSH();
+		~MSH();
+		!MSH();
 
-		bool WriteToFile(String^ Path)
-		{
-			return GetInstance()->WriteToFile(MarshalString(Path));
-		}
-
-		bool ReadFromFile(String^ Path)
-		{
-			return GetInstance()->ReadFromFile(MarshalString(Path));
-		}
-
-		property int ShadowVolume
-		{
-			int get()
-			{
-				return GetInstance()->m_SHVO.GetShadowVolume();
-			}
-		}
+		bool WriteToFile(String^ Path);
+		bool ReadFromFile(String^ Path);
+		property int ShadowVolume { int get(); };
 	};
 }
