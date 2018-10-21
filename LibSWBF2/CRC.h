2aaa7a3c9347fd5783332a192a29b3bcb04587da
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2018-10-21 16:31:17 +0200

    Added Zero CRC hash algorithm. Added full namespace path in LOG macro.

---
+++ LibSWBF2/CRC.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Logger.h"
+
+namespace LibSWBF2
+{
+	using Logging::ELogType;
+
+	class CRC
+	{
+	private:
+		static uint32_t table32[256];
+		static uint8_t toLower[256];
+
+	public:
+		LIBSWBF2_EXP static uint32_t CalcLowerCRC(const char* str);
+	};
+}
