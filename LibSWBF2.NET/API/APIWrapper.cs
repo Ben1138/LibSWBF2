308d6f336e818ee9c74a4a847a2d1ae92961c7cb
Ben1138 <marten.mildt@web.de>
2018-12-03 21:45:47 +0100

    Discard CppCLI wrapper for pure C# wrapper (not supported by Mono/Unity). Adapting Logging (WIP)

---
+++ LibSWBF2.NET/API/APIWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace LibSWBF2
+{
+    internal static class APIWrapper
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void LogCallback(IntPtr LoggerEntryPtr);
+
+
+        [DllImport("LibSWBF2")]
+        public static extern void LOG_SetCallbackMethod(LogCallback Callback);
+
+        [DllImport("LibSWBF2")]
+        public static extern void LOG_SetLogfileLevel(Logging.ELogType LogfileLevel);
+
+
+        [DllImport("LibSWBF2")]
+        public static extern IntPtr MSH_Create();
+
+        [DllImport("LibSWBF2")]
+        public static extern void MSH_Delete(IntPtr msh);
+
+        [DllImport("LibSWBF2")]
+        public static extern bool MSH_ReadFromFile(IntPtr msh, [MarshalAs(UnmanagedType.LPStr)] string path);
+    }
+}
