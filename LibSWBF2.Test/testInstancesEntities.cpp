2ab7f1fcf3cea8cad5b26a0123ceec486a5c62bb
William Herald Snyder <will.herald.snyder@gmail.com>
2020-01-04 22:53:25 -0500

    GetOverriddenProperties done and tested w/Unity importer, TODO: Fix TADA overrun

--- LibSWBF2.Test/testInstancesEntities.cpp
+++ LibSWBF2.Test/testInstancesEntities.cpp
@@ -21,11 +21,29 @@ int main(int ac, char **av)
 
 	COUT("Listing all entities:");
 
+	List<String> values;
+	List<uint32_t> properties;
+
+
 	for (int i = 0; i < entities.Size(); i++)
 	{
-		COUT("Found entity type: " << entities[i].GetTypeName().Buffer() 
+		COUT("\nFound entity type: " << entities[i].GetTypeName().Buffer() 
 			<< " which is subclass of " << entities[i].GetBaseName().Buffer()
 		);
+
+		COUT("  Overridden properties: ");
+
+		if (entities[i].GetOverriddenProperties(properties, values))
+		{
+			for (int j = 0; j < properties.Size(); j++)
+			{
+				COUT(fmt::format("\tHash: {}, Value: {}", properties[j], values[j].Buffer()).c_str());
+			}
+		}
+		else 
+		{
+			COUT("\tFailed to get overridden properties!");
+		}
 	}
 
 	return 0;
