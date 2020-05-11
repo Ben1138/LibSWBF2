4ea516aeb45b3235ff95ebddb147d24189828c8e
Ben1138 <marten.mildt@web.de>
2020-05-11 17:51:15 +0200

    Make getters in BaseChunk const. Added some more getters.

--- LibSWBF2/BaseChunk.h
+++ LibSWBF2/BaseChunk.h
@@ -32,15 +32,17 @@ namespace LibSWBF2::Chunks
 		virtual void WriteToStream(FileWriter& stream);
 		virtual void ReadFromStream(FileReader& stream);
 
-		inline size_t GetDataPosition();
+		size_t GetPosition() const;
+		size_t GetDataPosition() const;
 		bool PositionInChunk(const size_t& CurrentPosition);
 		bool SkipChunk(FileReader& stream, const bool& printWarn = true);
 		void EnsureEnd(FileReader& stream);
 		//void ForwardToNextHeader(FileReader& stream);
 
-		ChunkHeader GetHeader();
-		ChunkSize GetDataSize();
-		ChunkSize GetAlignedSize();
+		ChunkHeader GetHeader() const;
+		ChunkSize GetDataSize() const;		// just the size of the data
+		ChunkSize GetAlignedSize() const;	// size of the data, including trailing bytes
+		ChunkSize GetFullSize() const;		// size of the whole chunk, including complete header and trailling bytes
 
 	protected:
 		// since these variables are critical
