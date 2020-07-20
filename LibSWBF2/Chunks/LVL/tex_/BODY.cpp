a5e1b7e19bac534b39016871da1557c8f518e006
William Herald Snyder <will.herald.snyder@gmail.com>
2020-07-19 21:59:02 -0400

    DXTexCrossPlat functions added

--- LibSWBF2/Chunks/LVL/tex_/BODY.cpp
+++ LibSWBF2/Chunks/LVL/tex_/BODY.cpp
@@ -2,10 +2,15 @@
 #include "BODY.h"
 #include "FMT_.h"
 #include "Logging/Logger.h"
-//#include "DirectX/DXHelpers.h"
+#include "DirectX/DXHelpers.h"
 #include "InternalHelpers.h"
 #include "FileReader.h"
 #include <algorithm>
+#include <cstring>
+
+#ifndef _WIN32
+#include "DXTexCrossPlat.h"
+#endif
 
 
 namespace LibSWBF2::Chunks::LVL::LVL_texture
@@ -24,7 +29,6 @@ namespace LibSWBF2::Chunks::LVL::LVL_texture
 
     void BODY::ReadFromStream(FileReader& stream)
     {
-#ifdef _WIN32        
         BaseChunk::ReadFromStream(stream);
         Check(stream);
 
@@ -57,27 +61,37 @@ namespace LibSWBF2::Chunks::LVL::LVL_texture
         width = std::max(width / div, (size_t)2);
         height = std::max(height / div, (size_t)2);
 
+        size_t dataSize = GetDataSize();
+        D3DFORMAT d3dFormat = fmt->p_Info->m_Format;
 
+        uint8_t* bytes = new uint8_t[dataSize];
 
-        p_Image = new DirectX::ScratchImage();
-        p_Image->Initialize2D(D3DToDXGI(fmt->p_Info->m_Format), width, height, 1, 1);
-        const DirectX::Image* img = p_Image->GetImage(0, 0, 0);
-
-        if (!stream.ReadBytes(img->pixels, GetDataSize()))
+        if (!stream.ReadBytes(bytes, dataSize))
         {
             LOG_ERROR("Reading data failed!");
             BaseChunk::EnsureEnd(stream);
             return;
         }
+
+#ifdef _WIN32
+        p_Image = new DirectX::ScratchImage();
+        p_Image->Initialize2D(D3DToDXGI(d3dFormat), width, height, 1, 1);
+        const DirectX::Image* img = p_Image->GetImage(0, 0, 0);
+        memcpy(img -> pixels, bytes, dataSize);
+#else
+        p_Image = new DXTexCrossPlat::CrossPlatImage(width, height, 
+                                                    d3dFormat, bytes,
+                                                    dataSize);
 #endif
 
-        BaseChunk::EnsureEnd(stream);
-        
+        delete bytes;
+
+        BaseChunk::EnsureEnd(stream);       
     }
+
 
     bool BODY::GetImageData(ETextureFormat format, uint16_t& width, uint16_t& height, const uint8_t*& data)
     {
-#ifdef _WIN32
         if (p_Image == nullptr)
         {
             LOG_WARN("Called GetImageData before reading!");
@@ -85,15 +99,18 @@ namespace LibSWBF2::Chunks::LVL::LVL_texture
             return false;
         }
 
+#ifdef _WIN32
+
         const DirectX::Image* img = p_Image->GetImage(0, 0, 0);
+
         if (img == nullptr)
         {
             LOG_WARN("Called GetImageData before reading!");
             data = nullptr;
             return false;
         }
-
         DXGI_FORMAT targetFormat = TextureFormatToDXGI(format);
+
         if (DirectX::IsCompressed(img->format))
         {
             DirectX::ScratchImage* result = new DirectX::ScratchImage();
@@ -126,8 +143,14 @@ namespace LibSWBF2::Chunks::LVL::LVL_texture
         width = (uint16_t)img->width;
         height = (uint16_t)img->height;
         data = img->pixels;
-        return data != nullptr;
-#endif        
-        return true;
+
+#else 
+        width = (uint16_t) p_Image->width;
+        height = (uint16_t) p_Image->height;
+        data = p_Image -> DumpRGBA();
+
+#endif //_WIN32
+
+        return data != nullptr;    
     }
 }
