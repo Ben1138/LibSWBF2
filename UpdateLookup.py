deedd59349565ec400173009f1d54e16ccd5af16
Ben1138 <marten.mildt@web.de>
2020-08-05 00:15:00 +0200

    UpdateLookup.py now also includes odf property names, layer and game mode names from ldx files; snd, wld and lyr file names and better logging. Updated lookup.csv

--- UpdateLookup.py
+++ UpdateLookup.py
@@ -3,22 +3,57 @@ import os
 import re
 import sys
 
+LookupFile = "lookup.csv"
 ModToolsPath = "F:\\BF2_ModTools"
 namelist = set()
 
-# Grab all ODF file names without extension
+def AddToList(item):
+    # strip first
+    item = item.strip()
+    # ignore template project stuff and comments
+    if not item.startswith('@') and not item.startswith("--") and not item.startswith("//") and not item.startswith(r"\\") and not item.startswith("#"):
+        namelist.add(item)
+
+# read current list, so we can actually add stuff manually to the
+# lookup file that doesn't get deleted once this script is run
+for i, line in enumerate(open(LookupFile)):
+    AddToList(line)
+
+# Grab all ODF file names without extension and additionally
+# crawl them for property names
+propertyReg = re.compile(r"^(\w*)\s*=\s*.*$")
 count = 0
+count2 = 0
 for path in Path(ModToolsPath).rglob("*.odf"):
     name = os.path.basename(path).replace(".odf", "")
-    namelist.add(name)
+    AddToList(name)
     count += 1
+    for i, line in enumerate(open(path)):
+        for match in propertyReg.finditer(line):
+            found = match.group(1)
+            if not found.startswith("//") and "//" in found:
+                print("NOOO: " + found)
+                print(match)
+                print(match.groups())
+                print("In File: %s at line: %d", path, i)
+            AddToList(match.group(1))
+            count2 += 1
 print("Found %d ODF files" % count)
+print("Found %d Property Names in ODF files" % count2)
+
+# Grab all req files
+count = 0
+for path in Path(ModToolsPath).rglob("*.req"):
+    name = os.path.basename(path).replace(".req", "")
+    AddToList(name)
+    count += 1
+print("Found %d REQ files" % count)
 
 # Grab all MSH files in assets\Animations without extension
 count = 0
 for path in Path(ModToolsPath + "\\assets\\Animations").rglob("*.msh"):
     name = os.path.basename(path).replace(".msh", "")
-    namelist.add(name)
+    AddToList(name)
     count += 1
 print("Found %d Animation MSH files" % count)
 
@@ -26,18 +61,54 @@ print("Found %d Animation MSH files" % count)
 nameReg = re.compile(r"Name\(\"(.*)\"\)")
 sampleReg = re.compile(r"Sample\(\"(.*)\"")
 count = 0
+count2 = 0
 for path in Path(ModToolsPath).rglob("*.snd"):
+    name = os.path.basename(path).replace(".snd", "")
+    AddToList(name)
+    count += 1
     for i, line in enumerate(open(path)):
         for match in nameReg.finditer(line):
-            namelist.add(match.group(1).strip())
-            count += 1
+            AddToList(match.group(1))
+            count2 += 1
         for match in sampleReg.finditer(line):
-            namelist.add(match.group(1).strip())
+            AddToList(match.group(1))
+            count2 += 1
+print("Found %d SND files" % count)
+print("Found %d Names and Samples in SND files" % count2)
+
+# Grab all wld (world) files
+count = 0
+for path in Path(ModToolsPath).rglob("*.wld"):
+    name = os.path.basename(path).replace(".wld", "")
+    AddToList(name)
+    count += 1
+print("Found %d WLD files" % count)
+
+# Grab all lyr (world layer) files
+count = 0
+for path in Path(ModToolsPath).rglob("*.lyr"):
+    name = os.path.basename(path).replace(".lyr", "")
+    AddToList(name)
+    count += 1
+print("Found %d LYR files" % count)
+
+# Grab all .ldx files and extract names from Layer() and GameMode() declarations
+layerReg = re.compile(r"Layer\(\"(.*)\"")
+gameModeReg = re.compile(r"GameMode\(\"(.*)\"")
+count = 0
+for path in Path(ModToolsPath).rglob("*.ldx"):
+    for i, line in enumerate(open(path)):
+        for match in layerReg.finditer(line):
+            AddToList(match.group(1))
             count += 1
-print("Found %d Names and Samples in SND files" % count)
+        for match in gameModeReg.finditer(line):
+            AddToList(match.group(1))
+            count += 1
+print("Found %d Layer and GameMode Names in LDX files" % count)
 
 namelist = sorted(namelist, key=lambda s: s.lower())
 
-file = open("lookup.csv", "w")
+file = open(LookupFile, "w")
 for name in namelist:
     file.write(name + "\n")
+file.close()
