72834ab0c02f4416b52c57c275f64169a8b242c8
Ben1138 <marten.mildt@web.de>
2020-08-07 15:20:47 +0200

    Added missing template declarations for all platforms. Speed up compile time by pre-compiling external dependencies.

--- LibSWBF2/Chunks/GenericChunk.cpp
+++ LibSWBF2/Chunks/GenericChunk.cpp
@@ -237,6 +237,9 @@ namespace LibSWBF2::Chunks
 	template struct LIBSWBF2_API GenericChunk<"BMAP"_m>;
 	template struct LIBSWBF2_API GenericChunk<"emo_"_m>;
 	template struct LIBSWBF2_API GenericChunk<"_pad"_m>;
+	template struct LIBSWBF2_API GenericChunk<"XFRM"_m>;
+	template struct LIBSWBF2_API GenericChunk<"inst"_m>;
+	template struct LIBSWBF2_API GenericChunk<"DTEX"_m>;
 
 	// string chunks (see STR.cpp)
 	template struct LIBSWBF2_API GenericChunk<"NAME"_m>;
@@ -251,12 +254,4 @@ namespace LibSWBF2::Chunks
 	template struct LIBSWBF2_API GenericChunk<"RTYP"_m>;
 	template struct LIBSWBF2_API GenericChunk<"BNAM"_m>;
 	template struct LIBSWBF2_API GenericChunk<"DTLX"_m>;
-
-#ifndef _WIN32
-	//Undef ref linker errors fix
-	template struct LIBSWBF2_API GenericChunk<"XFRM"_m>;
-	template struct LIBSWBF2_API GenericChunk<"inst"_m>;
-	template struct LIBSWBF2_API GenericChunk<"DTEX"_m>;
-
-#endif //_WIN32
 }
--- LibSWBF2/stdafx.h
+++ LibSWBF2/stdafx.h
@@ -6,5 +6,11 @@
 #include <windows.h>
 #endif
 
-// TODO: Place additional headers here
+// TODO: Place additional headers to precompile here
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/mat3x3.hpp>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include "DirectX/DXHelpers.h"
 #include "req.h"
