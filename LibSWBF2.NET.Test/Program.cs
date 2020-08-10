88863a916791d2a7ec4548f350cbc50754bd0681
Ben1138 <marten.mildt@web.de>
2020-08-11 01:40:31 +0200

    Invalidate C# Wrapper instances once the corresponding native instance is destroyed.

--- LibSWBF2.NET.Test/Program.cs
+++ LibSWBF2.NET.Test/Program.cs
@@ -13,25 +13,35 @@ namespace LibSWBF2.NET.Test
     {
         static void Main(string[] args)
         {
-            Logger.SetLogLevel(ELogType.Warning);
-            Logger.OnLog += (LoggerEntry logEntry) => 
+            Model lastModel = null;
+
             {
-                Console.WriteLine(logEntry.ToString());
-            };
+                Logger.SetLogLevel(ELogType.Warning);
+                Logger.OnLog += (LoggerEntry logEntry) => 
+                {
+                    Console.WriteLine(logEntry.ToString());
+                };
 
-            Console.WriteLine("Loading... This might take a while...");
-            Level level = Level.FromFile(@"F:\SteamLibrary\steamapps\common\Star Wars Battlefront II\GameData\data\_lvl_pc\geo\geo1.lvl");
+                Console.WriteLine("Loading... This might take a while...");
+                Level level = Level.FromFile(@"F:\SteamLibrary\steamapps\common\Star Wars Battlefront II\GameData\data\_lvl_pc\geo\geo1.lvl");
 
-            Console.WriteLine("Is World Level: " + level.IsWorldLevel);
-            Console.WriteLine("Models:");
+                Console.WriteLine("Is World Level: " + level.IsWorldLevel);
+                Console.WriteLine("Models:");
 
-            Model[] models = level.GetModels();
-            foreach (Model model in models)
-            {
-                Console.WriteLine("\t" + model.Name);
+                Model[] models = level.GetModels();
+                foreach (Model model in models)
+                {
+                    Console.WriteLine("\t" + model.Name);
+                    lastModel = model;
+                }
+
+                //level.Delete();
+                Console.WriteLine("Done!");
             }
 
-            Console.WriteLine("Done!");
+            // Since we explicitly deleted the Level, all model references should be
+            // invalid at this point
+            Console.WriteLine("Last Model is still valid: " + lastModel.IsValid());
             Console.ReadKey();
         }
     }
