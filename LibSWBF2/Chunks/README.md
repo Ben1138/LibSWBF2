2afe39fb0245449b5d2be72459e395d79f5861b6
William Herald Snyder <will.herald.snyder@gmail.com>
2020-08-13 10:10:50 -0400

    Light wrapper class filled out, lght -> Light + Light list added to Level wrapper, full project CMake

--- LibSWBF2/Chunks/README.md
+++
@@ -1,7 +0,0 @@
-Class Documentation
-
-
-
-- Base Chunk
-
-
--- LibSWBF2/Wrappers/Light.cpp
+++ LibSWBF2/Wrappers/Light.cpp
@@ -1,54 +1,48 @@
 #include "Light.h"
-/*
+
 #include <string>
 #include <iostream>
-
 #define COUT(x) std::cout << x << std::endl
+
 
 namespace LibSWBF2::Wrappers
 {
 
 using namespace LibSWBF2::Chunks::LVL::common;
 
-BaseLight::BaseLight(DATA& description, SCOP& body){
+
+Light::Light(DATA& description, SCOP& body){
     
     const uint8_t *nameStr;
     size_t size;
     description.GetData(nameStr, size);
     
-    name = std::string(reinterpret_cast<const char *>(nameStr), size);
+    m_Name = std::string(reinterpret_cast<const char *>(nameStr), size);
     
     const auto children = body.GetChildren();
     
-    COUT(name);
+    COUT(m_Name);
 }
 
 
 
 OmnidirectionalLight::OmnidirectionalLight(DATA& description, SCOP& body) :
-					BaseLight(description, body) {
-
-//int radius;
+					Light(description, body) {
+    int radius = 0;
 }
 
-*/
 
-/*
 SpotLight::SpotLight(DATA& description, SCOP& body) :
-					BaseLight(description, body) {
-
-
-//int innerAngle, outerAngle;
+					Light(description, body) {
+    int innerAngle = 0;
+    int outerAngle = 0;
 }
-
 
 
 DirectionalLight::DirectionalLight(DATA& description, SCOP& body) :
-					BaseLight(description, body) {
-
-	//int length;
+					Light(description, body) {
+	int length = 0;
 }
 
-*/
 
-//}
+}
