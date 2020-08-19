b27acab9fc5a0f1b25fcf4ebe902acb26f67831a
William Herald Snyder <will.herald.snyder@gmail.com>
2020-08-19 14:38:47 -0400

    Basic terrain test

---
+++ LibSWBF2.NET/Wrappers/Terrain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using LibSWBF2.Logging;
+
+namespace LibSWBF2.Wrappers
+{
+    public class Model : NativeWrapper
+    {
+        internal Model(IntPtr modelPtr) : base(modelPtr)
+        {
+
+        }
+
+        public string Name
+        {
+            get 
+            {
+                if (!IsValid()) throw new Exception("Underlying native class is destroyed!");
+                return APIWrapper.Terrain_GetTerrainTexName(NativeInstance); 
+            }
+        }
+
+        public bool IsSkeletalMesh
+        {
+            get 
+            {
+                if (!IsValid()) throw new Exception("Underlying native class is destroyed!");
+                return APIWrapper.Model_IsSkeletalMesh(NativeInstance); 
+            }
+        }
+
+        // TODO: swap IntPtr with actualy wrapper class
+        public IntPtr[] GetSegments()
+        {
+            if (!IsValid()) throw new Exception("Underlying native class is destroyed!");
+
+            APIWrapper.Model_GetSegments(NativeInstance, out IntPtr segmentArr, out uint segmentCount);
+            IntPtr[] segments = new IntPtr[segmentCount];
+            Marshal.Copy(segmentArr, segments, 0, (int)segmentCount);
+            return segments;
+        }
+
+        // TODO: swap IntPtr with actualy wrapper class
+        public IntPtr[] GetSkeleton()
+        {
+            if (!IsValid()) throw new Exception("Underlying native class is destroyed!");
+
+            APIWrapper.Model_GetSkeleton(NativeInstance, out IntPtr boneArr, out uint boneCount);
+            IntPtr[] bones = new IntPtr[boneCount];
+            Marshal.Copy(boneArr, bones, 0, (int)boneCount);
+            return bones;
+        }
+    }
+}
