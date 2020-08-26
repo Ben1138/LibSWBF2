0baef20c70d9a00a3f64eb68e3e010b227ef620f
Ben1138 <marten.mildt@web.de>
2020-08-26 14:41:29 +0200

    Account for different library names on Windows, Linux and Mac.

--- LibSWBF2.NET/API/APIWrapper.cs
+++ LibSWBF2.NET/API/APIWrapper.cs
@@ -1,59 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.InteropServices;
-using System.Security;
-using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace LibSWBF2
 {
     internal static class APIWrapper
     {
+#if WIN32
+        const string LIB_NAME = "LibSWBF2";
+#else
+        const string LIB_NAME = "SWBF2";
+#endif
+
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void LogCallback(IntPtr LoggerEntryPtr);
 
 
         // Logging //
 
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern void LOG_SetCallbackMethod(LogCallback Callback);
 
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern void LOG_SetLogfileLevel(Logging.ELogType LogfileLevel);
 
 
         // Level //
 
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr Level_FromFile([MarshalAs(UnmanagedType.LPStr)] string path);
 
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern void Level_Destroy(IntPtr level);
 
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.U1)]
         public static extern bool Level_IsWorldLevel(IntPtr level);
         
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern void Level_GetModels(IntPtr level, out IntPtr modelArr, out uint modelCount);
 
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr Level_GetModel(IntPtr level, [MarshalAs(UnmanagedType.LPStr)] string modelName);
 
         // Model //
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.LPStr)]
         public static extern string Model_GetName(IntPtr model);
         
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern void Model_GetSegments(IntPtr model, out IntPtr segmentArr, out uint segmentCount);
 		
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.U1)]
         public static extern bool Model_IsSkeletalMesh(IntPtr model);
         
-        [DllImport("LibSWBF2", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.U1)]
         public static extern bool Model_GetSkeleton(IntPtr model, out IntPtr boneArr, out uint boneCount);
     }
