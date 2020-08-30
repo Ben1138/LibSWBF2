d1a01671143315bb951fdbe01df40b0e6a66405a
William Herald Snyder <will.herald.snyder@gmail.com>
2020-08-30 14:36:13 -0400

    IntPtr to T array function created for getting wrapper lists + Utils folder

--- LibSWBF2.NET/Wrappers/Terrain.cs
+++ LibSWBF2.NET/Wrappers/Terrain.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using LibSWBF2.Logging;
+using LibSWBF2.Utils;
 
 namespace LibSWBF2.Wrappers
 {
@@ -15,32 +16,19 @@ namespace LibSWBF2.Wrappers
 
         }
 
+        public Terrain() : base(IntPtr.Zero){}
+
+
         public int width;
         public int height;
 
-        public List<string> Names
+        public List<string> TextureNames
         {
             get 
             {
                 if (!IsValid()) throw new Exception("Underlying native class is destroyed!");
-                APIWrapper.Terrain_GetTexNames(NativeInstance, out uint numTextures, out IntPtr strings);
-
-                List<string> textureNames = new List<string>();
-
-                if (numTextures > 0)
-                {
-                    IntPtr[] stringPointers = new IntPtr[numTextures];
-                    Marshal.Copy(strings, stringPointers, 0, (int) numTextures);
-
-                    for (uint i = 0; i < numTextures; i++)
-                    {
-                        string texName = Marshal.PtrToStringAnsi(stringPointers[i]);
-                        textureNames.Add(texName);
-                    }
-
-                }  
-
-                return textureNames;
+                APIWrapper.Terrain_GetTexNames(NativeInstance, out uint numTextures, out IntPtr stringsPtr);
+                return MemUtils.ptrToStringList(stringsPtr, (int) numTextures);
             }
         }
 
@@ -74,18 +62,6 @@ namespace LibSWBF2.Wrappers
                 return heights;
             }
         }
-        
-        /*
-        public float[] GetHeights(out uint width, out uint height, out IntPtr heightsNative)
-        {
-            if (!IsValid()) throw new Exception("Underlying native class is destroyed!");
-            APIWrapper.Terrain_GetHeights(NativeInstance, out uint width, out uint height, out IntPtr heightsNative);
-
-            float[] heights = new float[(int) 256 * 81];
-            Marshal.Copy(heightsNative, heights, 0, (int) heights.Length);
-            return heights;
-        }
-        */
 
         public int[] Indicies
         {
