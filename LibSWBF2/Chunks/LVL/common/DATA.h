b60f9bbf2ac7ecbdd2e4d8e77e2d521fa1eae11c
William Herald Snyder <will.herald.snyder@gmail.com>
2020-09-02 18:59:02 -0500

    New DATA subclasses, PeekDATATag added as static DATA method; now used in SCOP::ReadFromStream to determine DATA child's meaning before reading

--- LibSWBF2/Chunks/LVL/common/DATA.h
+++ LibSWBF2/Chunks/LVL/common/DATA.h
@@ -4,11 +4,22 @@
 #include "Chunks/GenericChunk.h"
 #include "Chunks/RawData.h"
 #include "Types/Enums.h"
+#include "FileReader.h"
 
 
 namespace LibSWBF2::Chunks::LVL::common
 {
 
-struct LIBSWBF2_API DATA : public GenericChunk<"DATA"_m> {};
+struct LIBSWBF2_API DATA : public GenericChunk<"DATA"_m> {
+
+	static uint32_t PeekDATATag(FileReader& stream)
+	{
+		stream.SkipBytes(8);
+		uint32_t result = stream.ReadUInt32();
+		stream.SetPosition(stream.GetPosition() - 12);
+		return result;
+	}
+
+};
 
 }
--- LibSWBF2/Chunks/LVL/lght/lght.SCOP.h
+++ LibSWBF2/Chunks/LVL/lght/lght.SCOP.h
@@ -3,10 +3,42 @@
 #include "stdafx.h"
 #include "Chunks/GenericChunk.h"
 #include "Chunks/LVL/common/DATA.h"
+#include "Chunks/LVL/lght/vec2.DATA.h"
 #include "Chunks/LVL/lght/vec3.DATA.h"
 #include "Chunks/LVL/lght/vec4.DATA.h"
+#include "Chunks/LVL/lght/float.DATA.h"
+
+
+
+
 #include "Chunks/LVL/lght/lighttype.DATA.h"
 #include "Types/List.h"
+
+
+/*
+
+Known field tags for DATA children in lght SCOP chunks, as they correspond to lgt/LGT files.
+The first 4 aren't listed here since they are universal and required...
+
+Local light SCOPs (in order of appearance):
+
+
+
+CastShadow() - 0xee0ae9e7
+Static() - 0x3bc290d2 
+Range(float range) - 0xd20cdcfa
+Cone(float innerAngle, float outerAngle) - 0x6a4b40dd (Spot specific)
+
+
+Global light SCOP:
+
+Light1(string name) - 0x3a6956db
+Light2(string name) - 0xa76756da
+top(3 ints) - 0x3cdc10a7
+bottom(3 ints) - 0x2a6ba74e
+
+*/
+
 
 using LibSWBF2::Chunks::LVL::common::DATA;
 
@@ -23,6 +55,9 @@ struct LIBSWBF2_API SCOP_LGHT : public GenericChunk<"SCOP"_m>{
     DATA_VEC3 *p_PositionChunk;
     DATA_LIGHTTYPE *p_TypeChunk;
     DATA_VEC3 *p_ColorChunk;
+
+    DATA_FLOAT *p_RangeChunk;
+    DATA_VEC2 *p_ConeChunk;
 };
 
 }
