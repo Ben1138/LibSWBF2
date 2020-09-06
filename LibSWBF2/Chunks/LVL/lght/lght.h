7d1eb0a8c9550897677859c76480da0935812ba9
William Herald Snyder <will.herald.snyder@gmail.com>
2020-09-05 22:34:39 -0400

    lght simplification, changes made to account for updated understading of later lght chunks...

--- LibSWBF2/Chunks/LVL/lght/lght.h
+++ LibSWBF2/Chunks/LVL/lght/lght.h
@@ -11,40 +11,6 @@
 #include "Chunks/LVL/lght/vec3.DATA.h"
 
 
-/*
-'lght' chunk spec
-
-Example with a single omnidirectional light
-
-'lght' (number of lights not explicitly stated, presumably derived from lght chunk length)
-
-	'name' (? 4 bytes, seems to signal start of light list, kind of redundant...)
-  
-    lightinstance   {
- 
-    'data' local light/global light flag (? bytes), name string (? bytes)
-
-    'scop' lighttype/chunklength (uint32) a8 = omni, f4 = spot, e0 = dir
-                                          c0 = omni (cast specular)
-        'data' rotation, 25 (28) bytes
-
-        'data' position, 21 (24) bytes
-
-        'data' 		  ?, 13 (16) bytes
-
-        'data'    color, 21 (24) bytes
-
-        'data'		  ?,  9 (12) bytes
-
-        'data'		  ?, 13 (16) bytes
-    }
- 
-    next lightinstance {
-    ...
-    ...
-    ...
-    }
-*/
 
 using namespace LibSWBF2::Chunks::LVL::common;
 
@@ -60,10 +26,7 @@ public:
 
     List<DATA_TAG *> p_LightTags;
     List<SCOP_LGHT *> p_LightBodies;
-    STR<"NAME"_m> *p_Marker; //unknown purpose
-    
-private:
-    inline static bool skip = false;
+    STR<"NAME"_m> *p_Marker; //unknown purpose, hashed layer name?
 };
 
 }
