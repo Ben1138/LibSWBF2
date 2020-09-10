b9a28547304feaff836df499aa8c348012a0d227
William Herald Snyder <will.herald.snyder@gmail.com>
2020-09-10 15:03:45 -0400

    Generalized terrin reading (max 4 layers), though PTCH ReadFromStream appears not to set VBUF's child parent properly... (HAS SEGFAULT)

--- LibSWBF2.Test/testTerrainSplatMap.cpp
+++ LibSWBF2.Test/testTerrainSplatMap.cpp
@@ -26,6 +26,26 @@ using LibSWBF2::Logging::LoggerEntry;
 void libLog(const LoggerEntry* log){ COUT(log->ToString().Buffer()); }
 
 
+uint8_t* GetFirstThreeLayersRGBA(uint8_t *blendMapData, int dim, int elementSize)
+{
+	uint8_t *imageData = new uint8_t[dim * dim * 4]();
+	int imageIndex, blendMapIndex;
+
+	for (int i = 0; i < dim * dim; i++)
+	{
+		imageIndex = i * 4;
+		blendMapIndex = i * elementSize;
+
+		imageData[imageIndex]   = blendMapData[blendMapIndex];
+		imageData[imageIndex+1] = blendMapData[blendMapIndex+1];
+		imageData[imageIndex+2] = blendMapData[blendMapIndex+2];
+		imageData[imageIndex+3] = 255;
+	}
+
+	return imageData;
+}
+
+
 
 int main()
 {
@@ -36,21 +56,23 @@ int main()
 	Level *testLVL2 = Level::FromFile("/Users/will/Desktop/terrainblendinglvls/TST_Tex3_Painted.lvl");
 	//Level *testLVL2 = Level::FromFile("/Users/will/Desktop/MLC.lvl");
 #else
-	Level *testLVL = Level::FromFile("/home/will/Desktop/geo1.lvl");
+	Level *testLVL1 = Level::FromFile("/home/will/Desktop/geo1.lvl");
+	Level *testLVL2 = Level::FromFile("/home/will/Desktop/hole.lvl");
+	//Level *testLVL = Level::FromFile("/home/will/Desktop/geo1.lvl");
 #endif
 
 
-	uint32_t width, height, elementSize;
-	uint8_t *imgData;
+	uint32_t dim, elementSize;
+	uint8_t *blendMapData;
 
 	const Terrain& terr1 = testLVL1 -> GetTerrains()[0];
-	terr1.GetSplatMap(width,height,elementSize,imgData);
-	stbi_write_png("testsplat_blended.png", width, height, 4, reinterpret_cast<void *>(imgData), width*4);
+	terr1.GetBlendMap(dim,elementSize,blendMapData);
+	stbi_write_png("testsplat_blended.png", dim, dim, 4, reinterpret_cast<void *>(GetFirstThreeLayersRGBA(blendMapData,dim,elementSize)), dim*4);
 
 
 	const Terrain& terr2 = testLVL2 -> GetTerrains()[0];
-	terr2.GetSplatMap(width,height,elementSize,imgData);
-	stbi_write_png("testsplat_painted.png", width, height, 4, reinterpret_cast<void *>(imgData), width*4);
+	terr2.GetBlendMap(dim,elementSize,blendMapData);
+	stbi_write_png("testsplat_painted.png", dim, dim, 4, reinterpret_cast<void *>(GetFirstThreeLayersRGBA(blendMapData,dim,elementSize)), dim*4);
 
 	return 0;
 }
