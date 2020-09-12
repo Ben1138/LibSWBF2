6dbd7ed4942a91ca5a30de128b42c21fa4ba24b4
William Herald Snyder <will.herald.snyder@gmail.com>
2020-09-12 17:23:39 -0400

    Holes detected using indexbuffer in GetHeightMap + new heightmap CPP test

---
+++ LibSWBF2.Test/testTerrainHeightMap.cpp
@@ -0,0 +1,78 @@
+#include "LibSWBF2.h"
+#include "FileWriter.h"
+#include "Chunks/LVL/LVL.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stbi/stb_image_write.h"
+
+using LibSWBF2::Types::String;
+using LibSWBF2::Types::List;
+
+using namespace LibSWBF2::Chunks::LVL;
+using namespace LibSWBF2::Wrappers;
+
+using LibSWBF2::Logging::Logger;
+using LibSWBF2::Logging::LoggerEntry;
+
+
+#define COUT(x) std::cout << x << std::endl
+
+
+
+void libLog(const LoggerEntry* log){ COUT(log->ToString().Buffer()); }
+
+
+uint8_t* GetHeightMapWithHolesRGBA(float *heightMapData, int dim)
+{
+	uint8_t *imageData = new uint8_t[dim * dim * 4]();
+	int imageIndex;
+
+	for (int i = 0; i < dim * dim; i++)
+	{
+		imageIndex = i * 4;
+		float curHeight = heightMapData[i];
+
+		if (curHeight < -.1)
+		{
+			imageData[imageIndex]   = 255;
+		}
+		else 
+		{	
+			uint8_t h = (uint8_t) (curHeight * 254.0f);
+			imageData[imageIndex]   = h;
+			imageData[imageIndex+1] = h;
+			imageData[imageIndex+2] = h;
+		}
+
+		imageData[imageIndex+3] = 255;
+	}
+
+	return imageData;
+}
+
+
+
+int main()
+{
+	Logger::SetLogCallback(&libLog);
+
+#ifdef __APPLE__
+	Level *testLVL = Level::FromFile("/Users/will/Desktop/geo1.lvl");
+#else
+	Level *testLVL = Level::FromFile("/home/will/Desktop/geo1.lvl");
+#endif
+
+	uint32_t dim, scale;
+	float *heightMapData;
+
+	const Terrain& terr = testLVL -> GetTerrains()[0];
+	terr.GetHeightMap(dim,scale,heightMapData);
+
+	stbi_write_png("height_test.png", dim, dim, 4, reinterpret_cast<void *>(GetHeightMapWithHolesRGBA(heightMapData,dim)), dim*4);
+
+	return 0;
+}
