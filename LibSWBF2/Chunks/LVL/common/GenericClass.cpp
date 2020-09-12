0788791bb26de82aaf9a9745cfcc7daff3690c98
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2020-09-12 23:58:33 +0200

    Added support for entc, ordc, wpnc and expc class chunks, aswell as their PROP children.

---
+++ LibSWBF2/Chunks/LVL/common/GenericClass.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include "GenericClass.h"
+#include "InternalHelpers.h"
+#include "FileReader.h"
+
+namespace LibSWBF2::Chunks::LVL::common
+{
+	template<uint32_t Header>
+	void GenericClass<Header>::RefreshSize()
+	{
+		THROW("Not implemented!");
+	}
+
+	template<uint32_t Header>
+	void GenericClass<Header>::WriteToStream(FileWriter& stream)
+	{
+		THROW("Not implemented!");
+	}
+
+	template<uint32_t Header>
+	void GenericClass<Header>::ReadFromStream(FileReader& stream)
+	{
+		BaseChunk::ReadFromStream(stream);
+		GenericChunk<Header>::Check(stream);
+		
+        while (GenericChunk<Header>::ThereIsAnother(stream))
+        {
+            ChunkHeader next = stream.ReadChunkHeader(true);
+			if (next == "BASE"_h)
+			{
+				GenericChunk<Header>::READ_CHILD(stream, p_Base);
+			}
+			else if (next == "TYPE"_h)
+			{
+				GenericChunk<Header>::READ_CHILD(stream, p_Type);
+			}
+			else if (next == "PROP"_h)
+			{
+				PROP* prop;
+				GenericChunk<Header>::READ_CHILD(stream, prop);
+				m_Properties.Add(prop);
+			}
+        }
+
+		BaseChunk::EnsureEnd(stream);
+	}
+
+	template<uint32_t Header>
+	String GenericClass<Header>::ToString()
+	{
+		return fmt::format(
+			"Base: {}\n\n"
+			"Type: {}\n",
+			p_Base->m_Text,
+			p_Type->m_Text
+		).c_str();
+	}
+
+
+	template struct LIBSWBF2_API GenericClass<"entc"_m>;
+	template struct LIBSWBF2_API GenericClass<"ordc"_m>;
+	template struct LIBSWBF2_API GenericClass<"wpnc"_m>;
+	template struct LIBSWBF2_API GenericClass<"expc"_m>;
+}
