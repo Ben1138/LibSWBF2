185f7fde54ad8012e16efec120a52be831cbdc14
Will Snyder <willdotsnyder@gmail.com>
2020-09-18 23:35:12 -0700

    MaterialsFlagsToString fix, relevant method added to Segment wrapper

--- LibSWBF2.NET/Types/Enums.cs
+++ LibSWBF2.NET/Types/Enums.cs
@@ -14,4 +14,27 @@ public enum LightType : uint
   Spot = 3, 
 }
 
+
+public enum  MaterialFlags : uint
+{
+	Normal = 1,
+	Hardedged = 2,
+	Transparent = 4,
+	Glossmap = 8,
+	Glow = 16,
+	BumpMap = 32,
+	Additive = 64,
+	Specular = 128,
+	EnvMap = 256,
+	VertexLighting = 512,
+	TiledNormalmap = 2048,
+	Doublesided = 65536,
+
+	Scrolling = 16777216,
+	Energy = 33554432,
+	Animated = 67108864,
+
+	AttachedLight = 134217728,
+}
+
 }
--- LibSWBF2/Types/Enums.cpp
+++ LibSWBF2/Types/Enums.cpp
@@ -107,7 +107,12 @@ namespace LibSWBF2
 			result += "AttachedLight, ";
 		}
 
-		result.resize(result.size() - 2);
+		size_t resultSize = result.size();
+		if (resultSize > 1)
+		{
+			result.resize(resultSize - 2); //failed on overflow when length == 1
+		}
+
 		result += "]";
 		return result.c_str();
 	}
