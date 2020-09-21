cb0fda229516b9c146d08ca65cc80dd4f36c2380
Will Snyder <will.herald.snyder@gmail.com>
2020-09-21 19:23:41 -0400

    Removed fmt call w/too many few format params, coll code cleanup (SEGFAULT FIX)

--- LibSWBF2/Chunks/LVL/coll/coll.cpp
+++ LibSWBF2/Chunks/LVL/coll/coll.cpp
@@ -25,37 +25,17 @@ namespace LibSWBF2::Chunks::LVL::coll
 
         READ_CHILD(stream, p_ChunkName);
         READ_CHILD(stream, p_NodeName);
-
         READ_CHILD(stream, p_Info);
         READ_CHILD(stream, p_Verts);
-
-        READ_CHILD(stream, p_SpatialDataStructure);
-
-        /*
-        //TEST ALWAYS PASSES
-        for (int i = 0; i < p_SpatialDataStructure -> m_Leaves.Size(); i++)
-        {
-        	List<uint16_t> &indexList = p_SpatialDataStructure -> m_Leaves[i] -> m_Indicies;
-        	
-        	for (int j = 0; j < indexList.Size(); j++)
-        	{
-        		List<Vector3> &verts = p_Verts -> m_Content;
-        		
-        		if (indexList[j] >= verts.Size())
-        		{
-        			LOG_WARN("INDEX OUT OF BOUNDS IN TREE LEAF");
-        		}
-        	}
-        }
-        */
+        READ_CHILD(stream, p_Tree);
 
 		BaseChunk::EnsureEnd(stream);
 	}
 
     Types::String coll::ToString() 
     {
-        return p_SpatialDataStructure -> ToString() + " Name: " + 
-        								p_ChunkName -> ToString() +
-        								fmt::format(" Num verts: {}", p_Verts -> m_Verts.Size()).c_str();
+        return p_Tree -> ToString()  +
+               " Name: " + p_ChunkName -> ToString() +
+        		fmt::format(" Num verts: {}", p_Verts -> m_Verts.Size()).c_str();
     }
 }
