306b4f7e1c473e8a4698e8b0a17803486c3ba2bb
Will Snyder <will.herald.snyder@gmail.com>
2020-09-22 17:00:37 -0400

    CollisionMesh C# Wrapper + relevant marshaling code (MONO CRASH ON EXIT)

--- LibSWBF2/API.h
+++ LibSWBF2/API.h
@@ -15,6 +15,7 @@ namespace LibSWBF2
 		class Level; 
 		class Model;
 		class Segment;
+		class CollisionMesh;
 		struct Bone;
 		class Terrain;
 		class Light;
@@ -69,6 +70,7 @@ namespace LibSWBF2
 		LIBSWBF2_API const void  Model_GetSegments(const Model* model, const Segment**& segmentArr, uint32_t& segmentCount);
 		LIBSWBF2_API uint8_t 	 Model_IsSkeletalMesh(const Model* model);
 		LIBSWBF2_API uint8_t 	 Model_GetSkeleton(const Model* model, Bone*& boneArr, uint32_t& boneCount);
+		LIBSWBF2_API const CollisionMesh* Model_GetCollisionMesh(const Model *model);
 
 		// Wrappers - Segment
 		LIBSWBF2_API const void    Segment_GetVertexBuffer(const Segment* segment, uint32_t& numVerts, float*& vertBuffer);
@@ -78,6 +80,12 @@ namespace LibSWBF2
 		LIBSWBF2_API const char*   Segment_GetMaterialTexName(const Segment* segment);
 		LIBSWBF2_API const int32_t Segment_GetTopology(const Segment* segment);
 		LIBSWBF2_API const char*   Segment_GetMaterialFlags(const Segment* segment);
+
+		// Wrappers - CollisionMesh
+		LIBSWBF2_API const void CollisionMesh_GetIndexBuffer(CollisionMesh *collMesh, uint32_t& count, int*& buffer);
+        LIBSWBF2_API const void CollisionMesh_GetVertexBuffer(const CollisionMesh *collMesh, uint32_t& count, float_t*& buffer);
+
+
 
 		// Enums
 		LIBSWBF2_API const char* ENUM_TopologyToString(ETopology topology);
