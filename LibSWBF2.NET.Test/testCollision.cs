884348524289933e78f88d5be0d4efbd01e39837
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2020-10-05 17:04:36 +0200

    Adapted C# wrapper to new logging paradigm. Removed false error logs for missing main container (can be null). Ensure return size in API for boolean. Removed testCall in Level.

--- LibSWBF2.NET.Test/testCollision.cs
+++ LibSWBF2.NET.Test/testCollision.cs
@@ -1,33 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using LibSWBF2.Logging;
+using System.Threading;
+using LibSWBF2.Logging;
 using LibSWBF2.Wrappers;
 
 namespace LibSWBF2.NET.Test
 {
     class Program
     {
-        static void Main(string[] args)
-        {      
-            /*      
-            Logger.SetLogLevel(ELogType.Warning);
-            Logger.OnLog += (LoggerEntry logEntry) => 
+        static ReaderWriterLockSlim m_Lock = new ReaderWriterLockSlim();
+        static bool m_bCatchLogs = true;
+
+        static void CatchLogs()
+        {
+            bool bRun;
+            do
             {
-                Console.WriteLine(logEntry.ToString());
-            };
-            */
+                while(Logger.GetNextLog(out LoggerEntry next))
+                {
+                    Console.WriteLine(next.ToString());
+                }
+
+                m_Lock.EnterReadLock();
+                bRun = m_bCatchLogs;
+                m_Lock.ExitReadLock();
+            }
+            while (bRun);
+        }
+
+        static void Main(string[] args)
+        {
+            Logger.SetLogLevel(ELogType.Warning);
+            Thread logThread = new Thread(new ThreadStart(CatchLogs));
+            logThread.Start();
 
             Console.WriteLine("Loading... This might take a while...");
-            
-            //Level level = Level.FromFile(@"F:\SteamLibrary\steamapps\common\Star Wars Battlefront II\GameData\data\_lvl_pc\geo\geo1.lvl");
-            //Level level = Level.FromFile(@"/Users/will/Desktop/MLC.lvl");
-            Level level = Level.FromFile(@"/home/will/Desktop/geo1.lvl");
-            //Level level = Level.FromFile(@"/home/will/Desktop/geo1.lvl");
-            //Level level = Level.FromFile(@"/Users/will/Desktop/geo1.lvl");
+            Level level = Level.FromFile("F:/SteamLibrary/steamapps/common/Star Wars Battlefront II/GameData/data/_lvl_pc/geo/geo1.lvl");
 
             Model[] models = level.GetModels();
             foreach (Model model in models)
@@ -40,6 +47,12 @@ namespace LibSWBF2.NET.Test
             }
 
             Console.WriteLine("Done!");
+
+            m_Lock.EnterWriteLock();
+            m_bCatchLogs = false;
+            m_Lock.ExitWriteLock();
+            logThread.Join();
+            Console.ReadKey();
         }
     }
 }
