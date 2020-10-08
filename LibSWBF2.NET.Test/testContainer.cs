81519c5ce75db733b9b1162448c0c0736803a0a4
Will Snyder <will.herald.snyder@gmail.com>
2020-10-08 12:49:22 -0400

    Windows test paths

--- LibSWBF2.NET.Test/testContainer.cs
+++ LibSWBF2.NET.Test/testContainer.cs
@@ -43,11 +43,14 @@ namespace LibSWBF2.NET.Test
 
             Console.WriteLine("Loading... This might take a while...");
 
-            string path1 = @"/home/will/Desktop/geo1.lvl";
-            string path2 = @"/home/will/Desktop/MLC.lvl";
-            
+            //string path1 = @"/home/will/Desktop/geo1.lvl";
+            //string path2 = @"/home/will/Desktop/MLC.lvl";
+
             //string path1 = @"/Users/will/Desktop/MLC.lvl";
             //string path2 = @"/Users/will/Desktop/geo1.lvl";
+
+            string path1 = @"C:\Program Files (x86)\Steam\steamapps\common\Star Wars Battlefront II\GameData\data\_lvl_pc\geo\geo1.lvl";
+            string path2 = @"C:\Program Files (x86)\Steam\steamapps\common\Star Wars Battlefront II\GameData\data\_lvl_pc\hot\hot1.lvl";
 
             //Level level = Level.FromFile(@"F:\SteamLibrary\steamapps\common\Star Wars Battlefront II\GameData\data\_lvl_pc\geo\geo1.lvl");
 
