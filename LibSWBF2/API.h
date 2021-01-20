e7b5f6e3f6275b86a5962aaa0a615a293c65351e
Will Snyder <will.herald.snyder@gmail.com>
2021-01-20 18:01:15 -0500

    Container wrapper deletion

--- LibSWBF2/API.h
+++ LibSWBF2/API.h
@@ -64,6 +64,7 @@ namespace LibSWBF2
         LIBSWBF2_API const void* Container_GetWrapper(Container* container, uint32_t type, const char *name); 
         LIBSWBF2_API const void Container_LoadLevels(Container* container);
 		LIBSWBF2_API const bool Container_IsDone(Container* container);
+		LIBSWBF2_API const bool Container_Delete(Container* container);
 
 		// Wrappers - Level
 		LIBSWBF2_API Level*  Level_FromFile(const char* path);
