7381700736e12ff2ee63918a78d4a32b9771004a
William Herald Snyder <will.herald.snyder@gmail.com>
2021-05-05 08:31:21 -0400

    Collision LEAF data is a triangle fan, not a strip. TriangleFanToTriangleList added to InternalHelpers.  C# tests fixed/updated, testScripts.cs added for checking what scripts an lvl contains.

---
+++ LibSWBF2.NET.Test/testScripts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using LibSWBF2.Logging;
+using LibSWBF2.Wrappers;
+using LibSWBF2.Types;
+
+namespace LibSWBF2.NET.Test
+{
+    class ScriptsTest
+    {
+        static int Main(string[] args)
+        {               
+            var tb = new TestBench();
+
+            var c = tb.LoadAndTrackContainer(new List<string>(args), out List<Level> lvls);
+
+
+            Level level = lvls[0];
+            if (level == null)
+            {
+                return -1;
+            }
+        
+            var scripts = level.Get<Script>();
+
+            foreach (var script in scripts)
+            {
+                Console.WriteLine(script.Name);
+
+                if (script.GetData(out IntPtr data, out uint size))
+                {
+                    Console.WriteLine("    Size: " + size.ToString() + " bytes...");
+                }
+            } 
+
+            return 1;            
+        }
+    }
+}
