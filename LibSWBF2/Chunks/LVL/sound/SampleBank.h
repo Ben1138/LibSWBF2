ef82b072cb29a15ab73971f9177b4f95ecbc1638
William Herald Snyder <will.herald.snyder@gmail.com>
2021-11-26 10:48:53 -0500

    Seems as though sound data chunks are more complex than originally assumed, created dedicated SampleBankData chunk which does the SoundClip data reads.  SampleBankData will skip a clip if it has a non-zero alias (from SoundFMExtractor).  This fixes all buggy effects in Unity

--- LibSWBF2/Chunks/LVL/sound/SampleBank.h
+++ LibSWBF2/Chunks/LVL/sound/SampleBank.h
@@ -1,15 +1,15 @@
 #pragma once
 #include "Chunks/GenericChunk.h"
-#include "Chunks/RawData.h"
 
 namespace LibSWBF2::Chunks::LVL::sound
 {
 	struct SampleBankInfo;
+	struct SampleBankData;
 
 	struct LIBSWBF2_API SampleBank : public GenericChunk<"SampleBank"_fnv>
 	{
 		SampleBankInfo *p_Info;
-		RawData<"Data"_fnv> *p_Data;
+		SampleBankData *p_Data;
 
 		void RefreshSize() override;
 		void WriteToStream(FileWriter& stream) override;
