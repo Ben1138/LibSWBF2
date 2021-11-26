cac9f04a520563de8f06453872211ff125404a4d
William Herald Snyder <will.herald.snyder@gmail.com>
2021-11-26 17:23:35 -0500

    WorldAnimationKey fields publicized

--- LibSWBF2.NET/Types/WorldAnimationKey.cs
+++ LibSWBF2.NET/Types/WorldAnimationKey.cs
@@ -10,11 +10,11 @@ namespace LibSWBF2.Types
     [StructLayout(LayoutKind.Sequential, Pack=1)]
     public struct WorldAnimationKey
     {
-        float Time;
-        Vector3 Value; // For rotations this is in radians, in .ANM files they are in degrees 
-        EWorldAnimKeyTransitionType TransitionType;
-        Vector3 EaseOut;
-        Vector3 EaseIn;
+        public float Time;
+        public Vector3 Value; // For rotations this is in radians, in .ANM files they are in degrees 
+        public EWorldAnimKeyTransitionType TransitionType;
+        public Vector3 EaseOut;
+        public Vector3 EaseIn;
 
         public override String ToString()
         {
