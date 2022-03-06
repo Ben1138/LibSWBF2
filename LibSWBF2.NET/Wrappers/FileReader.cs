dc272e2c830cd7d473bf6eb3d69ab62785303fb6
William Herald Snyder <will.herald.snyder@gmail.com>
2022-03-06 14:18:06 -0500

    FileReader marshaller + C# wrapper added

---
+++ LibSWBF2.NET/Wrappers/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibSWBF2.Wrappers
+{
+    public sealed class FileReader : NativeWrapper
+    {
+        public static FileReader FromFile(string Path, bool UseMemoryMapping = false)
+        {
+            return FromNative<FileReader>(APIWrapper.FileReader_FromFile(Path, UseMemoryMapping));
+        }
+
+        public IntPtr GetNativePtr()
+        {
+            CheckValidity();
+            return NativeInstance;
+        }
+
+        ~FileReader()
+        {
+            //CheckValidity();
+            APIWrapper.FileReader_Delete(NativeInstance);
+            Invalidate();
+        }
+    }
+}
