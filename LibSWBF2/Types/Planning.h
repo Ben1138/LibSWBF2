414cf9acaa4fa458dd9b6b813903ca9bc2e74409
William Herald Snyder <will.herald.snyder@gmail.com>
2022-04-09 22:04:31 -0400

    All Hub fields reversed, packed weights will remain in raw form and can be manually deleted after being marshalled

--- LibSWBF2/Types/Planning.h
+++ LibSWBF2/Types/Planning.h
@@ -12,10 +12,30 @@ namespace LibSWBF2::Types
 		Vector3 position;
 		float_t radius;
 
-		uint8_t layerWeights[5];
+		// Indices into ARCS, referenced by last 3 bytes of
+		// each quantized branchweight
+		uint8_t connectionIndices[8];
+
+		uint8_t connectionsPerLayer[5];
+
+		uint8_t * quantizedDataBuffer = nullptr;
 
 		String ToString() const;
 		void ReadFromStream(FileReader& stream, uint16_t count);
+
+		~Hub()
+		{
+			ReleaseDataBuffer();
+		}
+
+		void ReleaseDataBuffer()
+		{
+			if (quantizedDataBuffer != nullptr)
+			{
+				delete[] quantizedDataBuffer;
+				quantizedDataBuffer = nullptr;	
+			}
+		}
 	};
 
 
