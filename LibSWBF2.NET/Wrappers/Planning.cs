e45af5c5d19b55f6442dcc54e9ecfb73ad5846e3
William Herald Snyder <will.herald.snyder@gmail.com>
2022-04-10 22:43:14 -0400

    [WIP] Planning marshalling

---
+++ LibSWBF2.NET/Wrappers/Planning.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibSWBF2.Wrappers
+{
+    public sealed class Hub : NativeWrapper
+    {
+        public Name 
+        {
+            get 
+            {
+                CheckValidity();
+                return Marshal
+            }
+        }
+
+        public Vector3 Position
+        {
+            get 
+            {
+                CheckValidity();
+                return //
+            }   
+        }
+
+        public float Radius
+        {
+            get
+            {
+                CheckValidity();
+                return //
+            }
+        }
+
+        public byte[] ConnectionIndices
+        {
+            get 
+            {
+                CheckValidity();
+                return //
+            }
+        }
+
+        public byte[] ConnectionsPerLayer
+        {
+            get
+            {
+                CheckValidity();
+
+            }
+        }
+
+        public byte[] QuantizedData
+        {
+            get 
+            {
+                CheckValidity();
+            }
+        }
+
+        Hub(){}
+    }
+
+
+
+
+    public sealed class Connection : NativeWrapper
+    {
+        public Name 
+        {
+            get 
+            {
+                CheckValidity();
+                return Marshal
+            }
+        }
+
+        public byte Start
+        {
+            get 
+            {
+                CheckValidity();
+                return //
+            }   
+        }
+
+        public byte End
+        {
+            get
+            {
+                CheckValidity();
+                return //
+            }
+        }
+
+        public uint Flag1
+        {
+            get 
+            {
+                CheckValidity();
+                return //
+            }
+        }
+
+        public uint Flag2
+        {
+            get
+            {
+                CheckValidity();
+
+            }
+        }
+
+        Connection(){}
+    }
+
+
+
+
+
+
+    public sealed class PlanSet : NativeWrapper
+    {
+        public Hub[] GetHubs()
+        {
+
+        }
+
+        public Connection[] GetConnections()
+        {
+
+        }
+
+        PlanSet(){}
+    }
+}
