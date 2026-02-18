c71d3aceea945e69e7a9e4b67e92b653985c58da
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2026-02-18 11:05:45 +0100

    Hardened UpdateLookup.py and removed packaging python script with 7z.

--- UpdateLookup.py
+++ UpdateLookup.py
@@ -3,9 +3,10 @@ import os
 import re
 import sys
 
-LookupFile = "lookup.csv"
-ModToolsPath = "F:\\BF2_ModTools"
+LookupFile = "./lookup.csv"
+ModToolsPath = "/mnt/hdd1/BF2_ModTools"
 namelist = set()
+
 
 def AddToList(item):
     # strip first
@@ -17,10 +18,13 @@ def AddToList(item):
         # to avoid case sensitive duplicates
         namelist.add(item.lower())
 
+
 # read current list, so we can actually add stuff manually to the
 # lookup file that doesn't get deleted once this script is run
-for i, line in enumerate(open(LookupFile)):
-    AddToList(line)
+if Path(LookupFile).is_file():
+    for i, line in enumerate(open(LookupFile)):
+        AddToList(line)
+
 
 # Grab all ODF file names without extension and additionally
 # crawl them for property names
@@ -31,13 +35,18 @@ for path in Path(ModToolsPath).rglob("*.odf"):
     name = os.path.basename(path).replace(".odf", "")
     AddToList(name)
     count += 1
-    for i, line in enumerate(open(path)):
-        for match in propertyReg.finditer(line):
-            found = match.group(1)
-            AddToList(match.group(1))
-            count2 += 1
+    try:
+        for i, line in enumerate(open(path)):
+            for match in propertyReg.finditer(line):
+                found = match.group(1)
+                AddToList(match.group(1))
+                count2 += 1
+    except Exception as e:
+        print(f"Error for '{path}': {e}")
+
 print("Found %d ODF files" % count)
 print("Found %d Property Names in ODF files" % count2)
+
 
 # Grab all req files and extract strings
 count = 0
@@ -47,12 +56,17 @@ for path in Path(ModToolsPath).rglob("*.req"):
     name = os.path.basename(path).replace(".req", "")
     AddToList(name)
     count += 1
-    for i, line in enumerate(open(path)):
-        for match in stringReg.finditer(line):
-            AddToList(match.group(1))
-            count2 += 1
+    try:
+        for i, line in enumerate(open(path)):
+            for match in stringReg.finditer(line):
+                AddToList(match.group(1))
+                count2 += 1
+    except Exception as e:
+        print(f"Error for '{path}': {e}")
+
 print("Found %d REQ files" % count)
 print("Found %d strings in REQ files" % count2)
+
 
 # Grab all MSH files in assets\Animations without extension
 count = 0
@@ -60,7 +74,9 @@ for path in Path(ModToolsPath + "\\assets\\Animations").rglob("*.msh"):
     name = os.path.basename(path).replace(".msh", "")
     AddToList(name)
     count += 1
+
 print("Found %d Animation MSH files" % count)
+
 
 # Grab all .snd files and extract names from Name() and Sample() declarations
 nameReg = re.compile(r"Name\(\"(.*)\"\)")
@@ -71,15 +87,21 @@ for path in Path(ModToolsPath).rglob("*.snd"):
     name = os.path.basename(path).replace(".snd", "")
     AddToList(name)
     count += 1
-    for i, line in enumerate(open(path)):
-        for match in nameReg.finditer(line):
-            AddToList(match.group(1))
-            count2 += 1
-        for match in sampleReg.finditer(line):
-            AddToList(match.group(1))
-            count2 += 1
+    try:
+        for i, line in enumerate(open(path)):
+            for match in nameReg.finditer(line):
+                AddToList(match.group(1))
+                count2 += 1
+            for match in sampleReg.finditer(line):
+                AddToList(match.group(1))
+                count2 += 1
+    except Exception as e:
+        print(f"Error for '{path}': {e}")
+
+
 print("Found %d SND files" % count)
 print("Found %d Names and Samples in SND files" % count2)
+
 
 # Grab all wld (world) files
 count = 0
@@ -87,7 +109,9 @@ for path in Path(ModToolsPath).rglob("*.wld"):
     name = os.path.basename(path).replace(".wld", "")
     AddToList(name)
     count += 1
+
 print("Found %d WLD files" % count)
+
 
 # Grab all lyr (world layer) files
 count = 0
@@ -95,25 +119,31 @@ for path in Path(ModToolsPath).rglob("*.lyr"):
     name = os.path.basename(path).replace(".lyr", "")
     AddToList(name)
     count += 1
+
 print("Found %d LYR files" % count)
+
 
 # Grab all .ldx files and extract names from Layer() and GameMode() declarations
 layerReg = re.compile(r"Layer\(\"(.*)\"")
 gameModeReg = re.compile(r"GameMode\(\"(.*)\"")
 count = 0
 for path in Path(ModToolsPath).rglob("*.ldx"):
-    for i, line in enumerate(open(path)):
-        for match in layerReg.finditer(line):
-            AddToList(match.group(1))
-            count += 1
-        for match in gameModeReg.finditer(line):
-            AddToList(match.group(1))
-            count += 1
+    try:
+        for i, line in enumerate(open(path)):
+            for match in layerReg.finditer(line):
+                AddToList(match.group(1))
+                count += 1
+            for match in gameModeReg.finditer(line):
+                AddToList(match.group(1))
+                count += 1
+    except Exception as e:
+        print(f"Error for '{path}': {e}")
+
 print("Found %d Layer and GameMode Names in LDX files" % count)
 
-namelist = sorted(namelist, key=lambda s: s.lower())
 
 file = open(LookupFile, "w")
+namelist = sorted(namelist, key=lambda s: s.lower())
 for name in namelist:
     file.write(name + "\n")
 file.close()
